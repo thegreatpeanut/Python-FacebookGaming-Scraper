301515ec16364a4eb6e49ac90181f79feb2339ef
thegreatpeanut <46037700+thegreatpeanut@users.noreply.github.com>
2020-08-10 16:07:32 +0300

    Update pythonScraper.py

--- pythonScraper.py
+++ pythonScraper.py
@@ -13,6 +13,7 @@ from selenium.webdriver.support import expected_conditions as EC
 import locale
 locale.setlocale(locale.LC_ALL, '')
 
+
 MainURL = "https://www.facebook.com/thesauruspg/"
 VideosURL = "https://www.facebook.com/pg/thesauruspg/videos/?ref=page_internal"
 
@@ -20,6 +21,7 @@ VideosURL = "https://www.facebook.com/pg/thesauruspg/videos/?ref=page_internal"
 # VideosURL = input('Video Page URL:')
 
 # Open Firefox
+global driver
 driver = webdriver.Firefox()
 
 driver.get(MainURL)
@@ -73,19 +75,37 @@ time.sleep(1)
 
 driver.get(VideosURL)
 
-time.sleep(1)
+
+def do_scroll():
+
+    time.sleep(1)
+
+    driver.execute_script("window.scrollTo(0, 10000)")
+
+    time.sleep(5)
+
+    not_now_button = driver.find_element_by_id('expanding_cta_close_button')
+    not_now_button.click()
+
+    time.sleep(3)
 
 
-driver.execute_script("window.scrollTo(0, 10000)")
+
+do_scroll()
+
+# time.sleep(1)
 
 
-time.sleep(5)
-
-not_now_button = driver.find_element_by_id('expanding_cta_close_button')
-not_now_button.click()
+# driver.execute_script("window.scrollTo(0, 10000)")
 
 
-time.sleep(3)
+# time.sleep(5)
+
+# not_now_button = driver.find_element_by_id('expanding_cta_close_button')
+# not_now_button.click()
+
+
+# time.sleep(3)
 
 
 
@@ -111,13 +131,24 @@ time.sleep(3)
 
 video = driver.find_elements_by_class_name('bnpdmtie')
 
+element = driver.find_element_by_xpath('/html/body/div[1]/div[4]/div[1]/div/div/div[2]/div[2]/div/div[3]/div/div[1]/div/div[1]/div[4]/div/div/div[1]/div/div[1]/span[2]/span/span')
+driver.execute_script("""
+var element = arguments[0];
+element.parentNode.removeChild(element);
+""", element)
+
+element2 = driver.find_element_by_xpath('/html/body/div[1]/div[4]/div[1]/div/div/div[2]/div[2]/div/div[3]/div/div[1]/div/div[4]/div/div/div/div/div[2]/div/div[1]/div/div/div/div/div[2]/div/div/span/div/span[2]/span/span')
+driver.execute_script("""
+var element2 = arguments[0];
+element2.parentNode.removeChild(element2);
+""", element2)
 video_likes = driver.find_elements_by_class_name('pcp91wgn')
 
 
 
-# Iterate trough all elements  
+# Iterate trough all elements || Must do otherwise errors 
 	
-#video views - split list into AGE and VIEWS // make AGE/TIME  ?? Undecided
+#video views - split list into AGE and VIEWS // make AGE/TIME :: 
 # "a day ago" = 1
 # "a week ago" = 7
 # "a month ago" = 30
@@ -126,11 +157,61 @@ video_likes = driver.find_elements_by_class_name('pcp91wgn')
 
 video_age_views = [x.text for x in video]
 video.extend(video_age_views)
-video_age_views = [i for i in video_age_views if i]
+video_age_views = [i for i in video_age_views if i if not 'ago' in i]
+video_age_views = [w.replace("Views", "") for w in video_age_views]
+#removetable1 = str.maketrans('', '', '.')
+#video_age_views = [s.translate(removetable1) for s in video_age_views]
+#video_age_views = [w.replace("K", "") for w in video_age_views]
+#video_age_views = [w.replace("M", "") for w in video_age_views]
+#video_age_views = [w.replace(".", "") for w in video_age_views]
+
+k_val = "K"
+m_val = "M"
+
+# if any(k_val in s for s in video_age_views):
+#     video_age_views = [w.replace("K", "") for w in video_age_views]
+#     video_age_views = [float(i) for i in video_age_views]
+#     video_age_views = [elem * 1000 for elem in video_age_views]
+
+# elif any(m_val in x for x in video_age_views):
+#     video_age_views = [w.replace("M", "") for w in video_age_views]
+#     video_age_views = [float(i) for i in video_age_views]
+#     video_age_views = [elem * 1000000 for elem in video_age_views]
 
 
-# trying to find a way to quantify the date for each video
-# [w.replace("a day ago", "1").replace("a week ago", "7").replace("days ago", "").replace("weeks ago" , "").replace("months ago" , "") for w in video_age_views]
+## app 1
+e_list = [i.replace('K', 'e3').replace('M', 'e6') for i in video_age_views]
+values = [float(i) for i in e_list]
+video_age_views = [f'{int(i):,}' for i in values]
+
+
+
+
+
+     
+
+#video_likes_int = [float(i) for i in video_age_views]
+        
+
+
+
+# for i in range(0, len(video_age_views)):
+#     video_age_views[i] = int(video_age_views[i])
+#     video_age_views[i] = format(video_age_views[i], 'n')
+
+
+# inds_k = [ i for i, s in enumerate(video_age_views) if 'K' in s] + [len(video_age_views)]
+# div_1 = [video_age_views[inds_k[i]:inds_k[i+1]] for i in range(len(inds_k)-1)]
+
+# def divide_List(video_age_views):
+#     dct = {}
+#     for k_val in video_age_views:
+#         if k_val not in dct:
+#             dct[k_val] = k_val
+#     res = []
+#     for key in sorted(dct):
+#         res.append(dct[key])
+#     return res                
 
 print(video_age_views)
 
@@ -157,8 +238,8 @@ time.sleep(3)
 
 searchfor = ['ago']
 
-df = pd.DataFrame(video_age_views, columns=["Age_and_Views"])
-df = df[~df.Age_and_Views.str.contains('|'.join(searchfor))]
+df = pd.DataFrame(video_age_views, columns=["Views"])
+#df = df[~df.Age_and_Views.str.contains('|'.join(searchfor))]
 
 df1 = pd.DataFrame(parsed_video_likes, columns=["Video_Likes"])
 
@@ -166,7 +247,7 @@ df2 = pd.DataFrame([name], columns=['Name'], index =[0])
 
 df3 = pd.DataFrame([followers], columns=['Followers'], index=[0])
 
-df4 = pd.DataFrame([page_likes], columns=['Page Likes'], index=[0])
+df4 = pd.DataFrame([page_likes], columns=['Page_Likes'], index=[0])
 
 result = pd.concat([df2, df3, df4, df, df1], axis=1)
 
