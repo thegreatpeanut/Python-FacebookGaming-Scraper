4c7c5574bcc866b7571ed3e96189a8c0f9eab030
thegreatpeanut <46037700+thegreatpeanut@users.noreply.github.com>
2020-08-12 12:13:45 +0300

    Update pythonScraper.py

--- pythonScraper.py
+++ pythonScraper.py
@@ -1,6 +1,8 @@
 import time
 import csv
 import json
+import io
+import psycopg2
 from bs4 import BeautifulSoup
 import re
 import os
@@ -10,7 +12,10 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
 import locale
+from sqlalchemy import create_engine
 locale.setlocale(locale.LC_ALL, '')
 
 
@@ -25,6 +30,8 @@ global driver
 driver = webdriver.Firefox()
 
 driver.get(MainURL)
+
+
 
 time.sleep(1)
 
@@ -42,7 +49,7 @@ remove = " people follow this,"
 pattern = "[" + remove + "]"
 followers = re.sub(pattern, "", followers)
 followers = int(followers)
-followers = f'{followers:,}'
+#followers = f'{followers:,}'
 
 print(followers)
 
@@ -61,7 +68,7 @@ remove3 = 'k'
 pattern3 = "[" + remove3 + "]"
 page_likes = re.sub(pattern3, "" , page_likes)
 page_likes = int(page_likes)
-page_likes = f'{page_likes:,}'
+#page_likes = f'{page_likes:,}'
 
 print(page_likes)
 
@@ -75,58 +82,120 @@ driver.get(VideosURL)
 
 
 def do_scroll():
+    try:
+        time.sleep(1)
+        driver.maximize_window()
+        driver.execute_script("window.scrollTo(0, 10000)")
 
-    time.sleep(1)
+        time.sleep(5)
 
-    driver.execute_script("window.scrollTo(0, 10000)")
+        not_now_button = driver.find_element_by_id('expanding_cta_close_button')
+        not_now_button.click()
 
-    time.sleep(5)
+        time.sleep(3)
 
-    not_now_button = driver.find_element_by_id('expanding_cta_close_button')
-    not_now_button.click()
+        driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.HOME)
 
-    time.sleep(3)
+        time.sleep(1)
 
+        driver.execute_script("window.scrollTo(0, 5000)")
+    except NoSuchElementException:
+        print("No element found")
 
 
 do_scroll()
 
+time.sleep(3)
 
-# last_height = driver.execute_script("return document.body.scrollHeight")
+
+
+last_height = driver.execute_script("return document.body.scrollHeight")
 
 # while True:
    
 #     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
     
-#     time.sleep(3)
+#     time.sleep(7)
 
     
 #     new_height = driver.execute_script("return document.body.scrollHeight")
 #     if new_height == last_height:
+#         time.sleep(10)
 #         break
 #     last_height = new_height
+    
+
 
 
 #--------------------------------------------------------------------------------------------------------
-# init var for views and video likes / remove ALL text eventaully , only keep int / It's an array of elements
+
+
+
+
+try:
+    element = driver.find_element_by_css_selector('div.cbu4d94t:nth-child(3) > div:nth-child(2)')
+    driver.execute_script("""
+    var element = arguments[0];
+    element.parentNode.removeChild(element);
+    """, element)
+except NoSuchElementException:
+    print("No element found")
+
+try:
+    element2 = driver.find_element_by_css_selector('div.cjfnh4rs:nth-child(2)')
+    driver.execute_script("""
+    var element2 = arguments[0];
+    element2.parentNode.removeChild(element2);
+    """, element2)
+except NoSuchElementException:
+    print("No element2 found")
+
+try:
+    element3 = driver.find_element_by_css_selector('html#facebook body._4-u5._2yq.UIPage_LoggedOut.hasBanner._-kb._605a.b_c3pyn-ahh.gecko.win.x1.Locale_en_US.cores-gte4._19_u.hasCookieBanner.hasAXNavMenubar div#u_0_6._li div#globalContainer.uiContextualLayerParent div#content.fb_content.clearfix div div.clearfix div._1qkq._1ql0 div#content_container div div#js_2._2pie div div div.c9zspvje.cbu4d94t.j83agx80 div.ihqw7lf3.cbu4d94t.j83agx80 div.j83agx80.l9j0dhe7.k4urcfbm div.rq0escxv.l9j0dhe7.du4w35lb.hybvsw6c.ue3kfks5.pw54ja7n.uo3d90p7.l82x9zwi.ni8dbmo4.stjgntxs.k4urcfbm.sbcfpzgs div.cbu4d94t.j83agx80 div.n851cfcs')
+    driver.execute_script("""
+    var element3 = arguments[0];
+    element3.parentNode.removeChild(element3);
+    """, element3)
+except NoSuchElementException:
+    print("No element3 found")
+
+try:
+    element4 = driver.find_element_by_css_selector('div.cbu4d94t:nth-child(4) > div:nth-child(2)')
+    driver.execute_script("""
+    var element4 = arguments[0];
+    element4.parentNode.removeChild(element4);
+    """, element4)
+except NoSuchElementException:
+    print("No element4 found")
+
+try:
+    element6 = driver.find_element_by_css_selector('div.n851cfcs:nth-child(2)')
+    driver.execute_script("""
+    var element6 = arguments[0];
+    element6.parentNode.removeChild(element6);
+    """, element6)
+except NoSuchElementException:
+    print("No element6 found")
+
+
+
+    
+
+elem1 = delete_VAR(driver.find_element_by_css_selector('div.cbu4d94t:nth-child(3) > div:nth-child(2)'))
+elem1.delete_p()
+elem2 = delete_VAR("driver.find_element_by_css_selector('div.cjfnh4rs:nth-child(2)')")
+elem2.delete_p()
+elem3 = delete_VAR("driver.find_element_by_css_selector('html#facebook body._4-u5._2yq.UIPage_LoggedOut.hasBanner._-kb._605a.b_c3pyn-ahh.gecko.win.x1.Locale_en_US.cores-gte4._19_u.hasCookieBanner.hasAXNavMenubar div#u_0_6._li div#globalContainer.uiContextualLayerParent div#content.fb_content.clearfix div div.clearfix div._1qkq._1ql0 div#content_container div div#js_2._2pie div div div.c9zspvje.cbu4d94t.j83agx80 div.ihqw7lf3.cbu4d94t.j83agx80 div.j83agx80.l9j0dhe7.k4urcfbm div.rq0escxv.l9j0dhe7.du4w35lb.hybvsw6c.ue3kfks5.pw54ja7n.uo3d90p7.l82x9zwi.ni8dbmo4.stjgntxs.k4urcfbm.sbcfpzgs div.cbu4d94t.j83agx80 div.n851cfcs')")
+elem3.delete_p()
+elem4 = delete_VAR("driver.find_element_by_css_selector('div.cbu4d94t:nth-child(4) > div:nth-child(2)')")
+elem4.delete_p()
+elem5 = delete_VAR("driver.find_element_by_css_selector('div.n851cfcs:nth-child(2)')")
+elem5.delete_p()
+
 
 video = driver.find_elements_by_class_name('bnpdmtie')
-
-element = driver.find_element_by_xpath('/html/body/div[1]/div[4]/div[1]/div/div/div[2]/div[2]/div/div[3]/div/div[1]/div/div[1]/div[4]/div/div/div[1]/div/div[1]/span[2]/span/span')
-driver.execute_script("""
-var element = arguments[0];
-element.parentNode.removeChild(element);
-""", element)
-
-element2 = driver.find_element_by_xpath('/html/body/div[1]/div[4]/div[1]/div/div/div[2]/div[2]/div/div[3]/div/div[1]/div/div[4]/div/div/div/div/div[2]/div/div[1]/div/div/div/div/div[2]/div/div/span/div/span[2]/span/span')
-driver.execute_script("""
-var element2 = arguments[0];
-element2.parentNode.removeChild(element2);
-""", element2)
 video_likes = driver.find_elements_by_class_name('pcp91wgn')
-
-
 
 video_age_views = [x.text for x in video]
 video.extend(video_age_views)
@@ -137,8 +206,8 @@ m_val = "M"
 e_list = [i.replace('K', 'e3').replace('M', 'e6') for i in video_age_views]
 values = [float(i) for i in e_list]
 video_age_views = [f'{int(i):,}' for i in values]
-         
-
+removetable = str.maketrans('', '', ',')
+video_age_views = [s.translate(removetable) for s in video_age_views]
 print(video_age_views)
 
 
@@ -152,7 +221,7 @@ removetable = str.maketrans('', '', '.')
 parsed_video_likes = [s.translate(removetable) for s in parsed_video_likes]
 for i in range(0, len(parsed_video_likes)):
     parsed_video_likes[i] = int(parsed_video_likes[i])
-    parsed_video_likes[i] = format(parsed_video_likes[i], "n")	
+    #parsed_video_likes[i] = format(parsed_video_likes[i], "n")	
 
 
 print(parsed_video_likes)
@@ -164,22 +233,27 @@ time.sleep(3)
 
 searchfor = ['ago']
 
-df = pd.DataFrame(video_age_views, columns=["Views"])
+df = pd.DataFrame(video_age_views, columns=["Views"]).astype(int)
 #df = df[~df.Age_and_Views.str.contains('|'.join(searchfor))]
 
-df1 = pd.DataFrame(parsed_video_likes, columns=["Video_Likes"])
+df1 = pd.DataFrame(parsed_video_likes, columns=["Video_Likes"]).astype(int)
 
 df2 = pd.DataFrame([name], columns=['Name'], index =[0])
 
-df3 = pd.DataFrame([followers], columns=['Followers'], index=[0])
 
-df4 = pd.DataFrame([page_likes], columns=['Page_Likes'], index=[0])
+df3 = pd.DataFrame([int(followers)], columns=['Followers'], index=[0])
+df3 = df3.fillna(0).astype(int)
 
+df4 = pd.DataFrame([int(page_likes)], columns=['Page_Likes'], index=[0]).astype(int)
+df4 = df4.fillna(0).astype(int)
 result = pd.concat([df2, df3, df4, df, df1], axis=1)
 
-result = result.fillna(" ")
+#result = result.fillna(" ")
+
 
 result.to_csv('ParsedConcat.csv')
 
+engine = create_engine('postgresql://postgres:@localhost:5432/FacebookGaming')
+result.to_sql('FBGaming', engine)
 
 #driver.close()
